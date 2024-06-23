54ab976f5f21ab48dbc5310d573561b7b0fa2d63
ian-rossi <91165417+ian-rossi@users.noreply.github.com>
2024-06-23 17:25:29 -0300

    Mudando variável das requests no controller

--- app/controllers/work_experience_controller.rb
+++ app/controllers/work_experience_controller.rb
@@ -9,13 +9,13 @@ class WorkExperienceController < ApplicationController
   def index
     number_of_processors = Etc.nprocessors
     hydra = Typhoeus::Hydra.new(max_concurrency: number_of_processors - 1)
-    filtered_request_by_service = request_by_service
+    filtered_request_by_service = request_by_service(params)
     filtered_request_by_service.each_key { |req| hydra.queue(req) }
     hydra.run!
-    requests.reduce({}) { |acc, (req, service)| acc.merge(service.to_dto(req.response)) }
+    filtered_request_by_service.reduce({}) { |acc, (req, service)| acc.merge(service.to_dto(req.response)) }
   end
 
-  def request_by_service
+  def request_by_service(params)
     request_by_service = @work_experience_services.group_by { |service| service.get_request(params) }
     request_by_service.filter { |req, _| !req.nil? }
   end
